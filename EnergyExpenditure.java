0fb4166219de24115d0e3f4ceb66f810524546be
rsveinson <rsveinson@mts.net>
2022-11-14 12:31:18 -0600

    added the calculateBMR method

--- EnergyExpenditure.java
+++ EnergyExpenditure.java
@@ -2,27 +2,27 @@ import java.util.Scanner;
 /** 
  * ACS-1903 Assignment 1 Question 2
  * @Key 
-*/
+ */
 
 public class EnergyExpenditure{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
         // variables
         String qc;              // quit or contine
-        
+
         // personal infor input from kb
         double weight = 0.0;
         int age = 0;
         int height = 0;
         char gender = 'm';
         char activityLevel = 'A';
-        
+
         // calculated values
         double bmr = 0.0;               // basal medabolic rate
         double tdee = 0.0;              // total daily energy expenditure
         double activityFactor = 0.0;    // based on input code   
-        
+
         // do until user inputs quit
         do{
             // get personal info
@@ -31,82 +31,90 @@ public class EnergyExpenditure{
             height = scanner.nextInt();
             age = scanner.nextInt();
             gender = scanner.next().toLowerCase().charAt(0);  // have to extract the required char from the input string
-            
+
             // Display the activity level menu
             displayActivityLevelMenu();
-            
+
             // get the activity factor
             activityLevel = getActivityLevel(scanner);  // have to extract the required char from the input string
-            
+
             // look up activity factor
             activityFactor = lookUpAF(activityLevel);
-            
+
             /* calculate the bmr
              * based on the formula given in the 
              * assignment instructions
              */
-            if(gender == 'm'){
-                bmr = 66 + (13.7 * weight) + (5 * height) -(6.8 * age);
-            }//end gender m
-            else{
-                bmr = 655 + (9.6 * weight) + (1.8 * height) -(4.7 * age);
-            }// end gender not m
+    
+            bmr = calculateBMR(gender, weight, height, age);
             
             //calculate tdee
             tdee = calculateTDEE(bmr, activityFactor);
-            
+
             // output results
             printSummary(bmr, tdee);
-            
-            // wuit or continue
+
+            // quit or continue
             System.out.println("Do you want to do another? Yes/No");
             qc = scanner.next();
             qc = qc.toLowerCase();
         }// end q/c do-while loop
         while(qc.equals("yes"));
-        
+
         // closing message        
         System.out.println("end of program");
     }// end main
-    
+
     // ***** static methods ****
+    public static double calculateBMR(char gender, double weight, double height, int age){
+        double bmr = 0.0;
+
+        if(gender == 'm'){
+            bmr = 66 + (13.7 * weight) + (5 * height) -(6.8 * age);
+        }//end gender m
+        else{
+            bmr = 655 + (9.6 * weight) + (1.8 * height) -(4.7 * age);
+        }// end gender not m
+        return bmr;
+    }
+
     public static void printSummary(double bmr, double tdee){
         // print summarty result
         System.out.format("BMR: %.2f\n",bmr);
         System.out.format("TDEE: %.2f\n",tdee);
     } //n end print
-    
+
     public static double calculateTDEE(double bmr, double activityFactor){
         return bmr * activityFactor;
     }// end get tdee
-    
+
     public static double lookUpAF(char al){
         double af;
-        
+
         // normalize al to uppercase
         al = Character.toUpperCase(al);
-        
+
         switch(al){
             case 'A': af = 1.2;
-            break;
+                break;
             case 'B': af = 1.375;
-            break;
+                break;
             case 'C': af = 1.55;
-            break;
+                break;
             case 'D': af = 1.725;
-            break;
+                break;
             default: af = -1;
         }// end swtich
         return af;
     }// end lookUpAF
-    
+
     public static char getActivityLevel(Scanner s){
         String st = s.next();
         return st.charAt(0);
     }// end get actibity level
-    
+
     public static void displayActivityLevelMenu(){
-    // display menu
+        // display menu
         System.out.println("Enter your activity level:");
         System.out.println("[A]  Sedentary");
         System.out.println("[B]  Lightly Active");
