2630935087004e095495bf3b2144686debc2ac24
rsveinson <rsveinson@mts.net>
2022-11-26 11:05:02 -0600

    addded code to accompany the walktrhtough video

---
+++ ColumnAlignmentGen0.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+/** 
+ * ACS-1903 Assignment 2 Question 2
+ * @sveinson
+ * aligin input text according to the string type
+*/
+
+public class ColumnAlignmentGen0{
+    public static void main(String[] args) {
+        // vairables constants and objects
+        Scanner scanner = new Scanner(System.in);
+        
+        // get the column width
+        
+        // read data from a file
+        
+        // see if input string is too long
+        
+        // get numeric or not from a static method
+        
+        // build the output string for numeric and text strings
+        // if it's too long set stout = strin
+        // if it's numeric
+            // right align by padding the lefts side of the output string
+            // add strin
+        // if it's not numeric centre align
+            // pad left side of strou with dots
+            // add strin
+            // pad right side of strout
+        
+        // output the aligned string
+        
+        System.out.println("end of program");
+    }// end main
+}// end main
--- ColumnAlignmentGen1.java
+++ ColumnAlignmentGen1.java
@@ -30,7 +30,20 @@ public class ColumnAlignmentGen1{
         while(fin.hasNext()){ 
             // read the next line from the file
             strin = fin.nextLine();
-            System.out.println(strin);
+            //System.out.println(strin);
+
+            if(strin.length() > width){
+                System.out.println("too long");
+                strout = strin;
+            }// end too long
+            else{
+                if(isNumeric(strin)){
+                    System.out.println("numeric");
+                }// end is numeric
+                else{
+                    System.out.println("not numeric");
+                }// end not numeric
+            }// end ok
 
         }//end eof
 
@@ -38,5 +51,47 @@ public class ColumnAlignmentGen1{
         System.out.println("end of program");
     }// end main
 
-    
+    /* take in a string and return true if the string
+     * is numeric i.e. digits
+     * it may also be numeric if the first character is $
+     * and if there is one and only one . ie. a decimal point
+     */
+    public static boolean isNumeric(String strin){
+        // assume the string is numeric
+        boolean n = true;       // assume that the string is numeric
+        char ch;                // local variable for each character extracted from strin
+
+        int dotCount = 0;       // number of dots in the string
+        // see if the first character is anything but a digit or $
+        ch = strin.charAt(0);
+        //System.out.println(ch);
+
+        if(!(ch == '$' || Character.isDigit(ch))){
+            n = false;
+            //System.out.println("this is text");
+        }// end first character
+        
+        // iterate over the remaining characters
+        // count the dots
+        //if any of them are not digits and there are more than one .
+        // set numeric to false
+        for(int i = 1; i < strin.length() && n; i++){
+                ch = strin.charAt(i);   // extract each char in turn
+                //System.out.println(ch);
+                
+                if(!Character.isDigit(ch)){
+                    if(ch == '.' && dotCount < 1){
+                        dotCount++;
+                    }// end not digit and dot count < 1
+                    else{
+                        n = false;
+                    }// end not a digit not a dot
+
+                }// end not a digit
+            }// end for int i
+
+    // now we know if the input string is numeric or not
+    return n; 
+}// end isNumeric
+
 }// end class
